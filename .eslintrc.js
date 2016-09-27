11a6adbd8c6c8ff56bfe5900af3305fc9b63f997
Iheb KHEMISSI <iheb.khemissi@gmail.com>
2016-09-27 22:29:00 +0100

    style: Update code styling rules
    Remove unnecessary code styling rule exceptions

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,24 +4,10 @@ module.exports = {
     sourceType: 'script', // https://github.com/eslint/eslint/issues/5301
   },
   rules: {
-    'no-underscore-dangle': ['error',
-      {
-        allow: ['_id'],
-        allowAfterThis: true,
-        allowAfterSuper: true,
-      },
-    ],
     'max-len': ['error',
       {
         code: 200,
         tabWidth: 2,
-      },
-    ],
-    'no-console': 0,
-    'class-methods-use-this': 0,
-    'new-cap': ['error',
-      {
-        capIsNewExceptions: ['Router'], // express.Router
       },
     ],
   },
