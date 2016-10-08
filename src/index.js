b1b97407baf1ceb01edb16843eac40f1234cac04
Iheb KHEMISSI <iheb.khemissi@gmail.com>
2016-10-08 12:14:46 +0100

    feat: Add tag date to the tagged-version details
    Add commit date to the list of tagged-version details

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 const semver = require('semver');
 const childProcess = require('child-process-promise');
 
-const tagAndHashRegex = /.*tag:\s*([^,)]+).*;(.+)/;
+const commitDetailsRegex = /.*tag:\s*([^,)]+).*;(.+);(.+)/;
 
 function runCommand(command) {
   return childProcess.exec(command)
@@ -11,14 +11,15 @@ function runCommand(command) {
 }
 
 function getList(range) {
-  return runCommand('git log --no-walk --tags --pretty="%d;%H" --decorate=short')
+  return runCommand('git log --no-walk --tags --pretty="%d;%H;%cI" --decorate=short')
     .then(output => output.split('\n'))
-    .then(lines => lines.map(line => tagAndHashRegex.exec(line)))
-    .then(tags => tags.filter(tagAndHash => Array.isArray(tagAndHash) && tagAndHash.length === 3))
+    .then(lines => lines.map(line => commitDetailsRegex.exec(line)))
+    .then(tags => tags.filter(tagAndHash => Array.isArray(tagAndHash) && tagAndHash.length === 4))
     .then(tags => tags.map((details) => {
       const tag = details[1].trim();
       const hash = details[2].trim();
-      return { tag, hash, version: semver.valid(tag) };
+      const date = new Date(details[3].trim());
+      return { tag, hash, date, version: semver.valid(tag) };
     }))
     .then(tags => tags.filter(details => !!details.version))
     .then(tags => tags.filter(details => !range || semver.satisfies(details.version, range)))
--- test/index.spec.js
+++ test/index.spec.js
@@ -6,10 +6,10 @@ const childProcess = require('child-process-promise');
 const taggedVersions = require('../src');
 
 const gitLog = `
-HEAD -> master, tag: v1.2.0, origin/master, origin/HEAD;f6bf448b02c489c8676f2eeaaac72ef93980baf2
-tag: v1.1.1;b656238b0fc2502b19bd0e803eb87447840dc52a
-tag: v1.1.0;1d56b88b0fc2585ffaf43e416b87440667c3c53f
-tag: v1.0.0;06743d3f902b19bd0e802e40462d87ba2b05740d
+(HEAD -> master, tag: v1.2.0, origin/master, origin/HEAD);f6bf448b02c489c8676f2eeaaac72ef93980baf2;2016-10-08T11:47:01+01:00
+(tag: v1.1.1);cd88316d6976ce8878a90a1a6d4eb326d16a4d68;2016-10-01T17:34:24+01:00
+(tag: v1.1.0);61fca7630fbe863f8d890d7a33e9b45f786d79e8;2016-09-27T22:35:42+01:00
+(tag: v1.0.0);9c5d6e1930831431c005bc74543f61a5cb36d617;2016-09-26T23:56:00+01:00
 `;
 
 const versions = {
@@ -17,57 +17,61 @@ const versions = {
     version: '1.2.0',
     tag: 'v1.2.0',
     hash: 'f6bf448b02c489c8676f2eeaaac72ef93980baf2',
+    date: new Date('2016-10-08T11:47:01+01:00'),
   },
   '1.1.1': {
     version: '1.1.1',
     tag: 'v1.1.1',
-    hash: 'b656238b0fc2502b19bd0e803eb87447840dc52a',
+    hash: 'cd88316d6976ce8878a90a1a6d4eb326d16a4d68',
+    date: new Date('2016-10-01T17:34:24+01:00'),
   },
   '1.1.0': {
     version: '1.1.0',
     tag: 'v1.1.0',
-    hash: '1d56b88b0fc2585ffaf43e416b87440667c3c53f',
+    hash: '61fca7630fbe863f8d890d7a33e9b45f786d79e8',
+    date: new Date('2016-09-27T22:35:42+01:00'),
   },
   '1.0.0': {
     version: '1.0.0',
     tag: 'v1.0.0',
-    hash: '06743d3f902b19bd0e802e40462d87ba2b05740d',
+    hash: '9c5d6e1930831431c005bc74543f61a5cb36d617',
+    date: new Date('2016-09-26T23:56:00+01:00'),
   },
 };
 
 test.beforeEach(t => {
-    t.context.exec = childProcess.exec;
-    childProcess.exec = sinon.stub().returns(Promise.resolve({ stdout: gitLog }));
+  t.context.exec = childProcess.exec;
+  childProcess.exec = sinon.stub().returns(Promise.resolve({ stdout: gitLog }));
 });
 
 test.afterEach(t => {
-    childProcess.exec = t.context.exec;
+  childProcess.exec = t.context.exec;
 });
 
 test('return all tagged versions', (t) => {
-    return taggedVersions.getList()
-      .then((list) => {
-          t.deepEqual(list, [versions['1.2.0'], versions['1.1.1'], versions['1.1.0'], versions['1.0.0']]);
-      });
+  return taggedVersions.getList()
+    .then((list) => {
+      t.deepEqual(list, [versions['1.2.0'], versions['1.1.1'], versions['1.1.0'], versions['1.0.0']]);
+    });
 });
 
 test('return all tagged versions within a range', (t) => {
-    return taggedVersions.getList('^1.1.0')
-      .then((list) => {
-          t.deepEqual(list, [versions['1.2.0'], versions['1.1.1'], versions['1.1.0']]);
-      });
+  return taggedVersions.getList('^1.1.0')
+    .then((list) => {
+      t.deepEqual(list, [versions['1.2.0'], versions['1.1.1'], versions['1.1.0']]);
+    });
 });
 
 test('return last tagged version', (t) => {
-    return taggedVersions.getLastVersion()
-      .then((version) => {
-          t.deepEqual(version, versions['1.2.0']);
-      });
+  return taggedVersions.getLastVersion()
+    .then((version) => {
+      t.deepEqual(version, versions['1.2.0']);
+    });
 });
 
 test('return last tagged version within a range', (t) => {
-    return taggedVersions.getLastVersion('~1.1')
-      .then((version) => {
-          t.deepEqual(version, versions['1.1.1']);
-      });
+  return taggedVersions.getLastVersion('~1.1')
+    .then((version) => {
+      t.deepEqual(version, versions['1.1.1']);
+    });
 });
