7959e6e9e085207bda2f7d7cd7f9bdbc109db99e
Damien Lebrun <dinoboff@hotmail.com>
2017-01-18 01:34:22 +0000

    chore: refactor getList
    Ddecompose getList into named functions and  merge all sync promise handler into one.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/**
+ * Query github for semantic version tag.
+ *
+ * @typedef {{tag: string, version: string, hash: string, date: Date}} Tag
+ */
+
 'use strict';
 
 const semver = require('semver');
@@ -6,11 +12,24 @@ const childProcess = require('child-process-promise');
 const tagRegex = /tag:\s*([^,)]+)/g;
 const commitDetailsRegex = /.*?(tag:\s*([^,)]+).*);(.+);(.+)/;
 
+/**
+ * Run shell command and resolve with stdout content
+ *
+ * @param  {string} command Shell command
+ * @return {Promise<string,Error>}
+ */
 function runCommand(command) {
   return childProcess.exec(command)
     .then(result => result.stdout);
 }
 
+/**
+ * Return first semantic version tag name in the list reference.
+ *
+ * @param  {string} allTags Comma seprated list of tag name
+ * @param  {string} guess   Last tag match
+ * @return {string}
+ */
 function getFirstValidTag(allTags, guess) {
   let match;
   let result = guess;
@@ -20,7 +39,6 @@ function getFirstValidTag(allTags, guess) {
       break;
     }
   }
-
   return result;
 }
 
@@ -28,27 +46,92 @@ function isString(v) {
   return typeof v === 'string';
 }
 
+/**
+ * Parse commit into an array of tag object.
+ *
+ * @param  {string} line Line to parse
+ * @return {Tag}
+ */
+function parseLine(line) {
+  const match = commitDetailsRegex.exec(line);
+
+  if (!match || match.length < 5) {
+    return null;
+  }
+
+  const tag = getFirstValidTag(match[1], match[2]);
+  const version = semver.valid(tag);
+
+  if (!version) {
+    return null;
+  }
+
+  const hash = match[3].trim();
+  const date = new Date(match[4].trim());
+
+  return { tag, hash, date, version };
+}
+
+/**
+ * Filter tags with range.
+ *
+ * Skip filtering if the range is not set.
+ *
+ * @param  {Array<Tag>} tags  List of tags
+ * @param  {string}     range Semantic range to filter with.
+ * @return {Array<Tag>}
+ */
+function filterByRange(tags, range) {
+  if (!range) {
+    return tags;
+  }
+
+  return tags.filter(tag => semver.satisfies(tag.version, range));
+}
+
+/**
+ * Compare tag by version.
+ *
+ * @param  {Tag} a First tag
+ * @param  {Tag} b Second tag
+ * @return {number}
+ */
+function compareTag(a, b) {
+  return semver.rcompare(a.version, b.version);
+}
+
+/**
+ * Get list of tag with a  semantic version name.
+ *
+ * @param  {object|string} options       Options map or range string
+ * @param  {string}        options.range Semantic range to filter tag with
+ * @param  {string}        options.rev   Revision range to filter tag with
+ * @return {Promise<Array<Tag>,Error>}
+ */
 function getList(options) {
   const range = isString(options) ? options : (options && options.range);
   const rev = options && options.rev;
   const fmt = '--pretty="%d;%H;%ci" --decorate=short';
   const cmd = rev ? `git log --simplify-by-decoration ${fmt} ${rev}` : `git log --no-walk --tags ${fmt}`;
 
-  return runCommand(cmd)
-    .then(output => output.split('\n'))
-    .then(lines => lines.map(line => commitDetailsRegex.exec(line)))
-    .then(tags => tags.filter(tagAndHash => Array.isArray(tagAndHash) && tagAndHash.length === 5))
-    .then(tags => tags.map((details) => {
-      const tag = getFirstValidTag(details[1], details[2]);
-      const hash = details[3].trim();
-      const date = new Date(details[4].trim());
-      return { tag, hash, date, version: semver.valid(tag) };
-    }))
-    .then(tags => tags.filter(details => !!details.version))
-    .then(tags => tags.filter(details => !range || semver.satisfies(details.version, range)))
-    .then(tags => tags.sort((t1, t2) => semver.rcompare(t1.version, t2.version)));
+  return runCommand(cmd).then((output) => {
+    const lines = output.split('\n');
+    const tags = lines
+      .map(parseLine)
+      .filter(tag => tag != null);
+
+    return filterByRange(tags, range).sort(compareTag);
+  });
 }
 
+/**
+ * Get most recent tag.
+ *
+ * @param  {object|string} options       Options map or range string
+ * @param  {string}        options.range Semantic range to filter tag with
+ * @param  {string}        options.rev   Revision range to filter tag with
+ * @return {Promise<Tag,Error>}
+ */
 function getLastVersion(options) {
   return getList(options)
     .then(list => list[0]);
